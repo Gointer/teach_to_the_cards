b97f45dfbcc17475001ea3e5c7b4baeb73c12f1a
Vitalii <gointer@ukr.net>
2016-04-24 17:43:29 +0300

    Added url and str for card model.

--- card/models.py
+++ card/models.py
@@ -1,8 +1,15 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 
 class Card(models.Model):
 	question = models.TextField(unique=True)
 	answer = models.TextField()
+
+	def __str__(self):
+		return self.question
+
+	def get_absolute_url(self):
+		return reverse('detail', args=[self.id])
 
 	
